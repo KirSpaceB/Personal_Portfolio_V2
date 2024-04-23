33a301a574fb104d1fbf1f6fa068a63d07dff72f
KirSpaceB <productivekirk@gmail.com>
2024-04-22 20:04:16 -0700

    feat:landing page layout completed

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,9 +4,36 @@ export default function LandingPage() {
   const animatedString = useAnimatedString("Hello, world!", 100); // Example usage
 
   return (
-    <div className="flex-grow bg-red-500">
-      {animatedString}
+    <div className="flex flex-col justify-between bg-red-500">
+      <header className="bg-blue-500 w-screen h-16">
+
+      </header>
+      
+      <div className="flex flex-col justify-start">
+        <div className="flex flex-row items-center">
+          <div className="w-16 h-16 bg-blue-500 m-16">
+
+          </div>
+          <p className="w-[200px] overflow-hidden text-wrap" // Applying a fixed width to stabilize layout
+          >
+            {animatedString}
+          </p>
+        </div>
+
+        <div className="flex flex-row items-center">
+          <div className="w-16 h-16 bg-blue-500 m-16">
+
+          </div>
+          <p className="w-[200px] overflow-hidden text-wrap" // Applying a fixed width to stabilize layout
+          >
+            {animatedString}
+          </p>
+        </div>
+      </div>
+
+      <footer className="bg-blue-500 w-screen h-16">
+
+      </footer>
     </div>
   );
-
 }
